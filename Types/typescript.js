14c0feeb0b12d59bd7216614d822008a3ddb9aef
Crystal <crystal120599@gmail.com>
2023-12-13 08:52:20 -0800

    created project and worked on first content

---
+++ Types/typescript.js
@@ -0,0 +1,6 @@
+// Typescript code in files with the extension .ts
+/*Code is ran through the TS transpiler (takes input code and output as diff lang)
+If no errors it will be converted into JS file version
+*/
+// TS is a superset of JS, just new features
+// run tsc or tsc.cmd to run the transcompiler to create js file
